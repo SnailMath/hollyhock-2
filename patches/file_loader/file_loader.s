310e9b07cd2fd579698fe0b1c6ebedc3fc839ee6
SnailMath <snailmathprog@gmail.com>
2020-11-25 21:11:23 +0100

    Add exam mode detection

--- patches/file_loader/file_loader.s
+++ patches/file_loader/file_loader.s
@@ -14,6 +14,57 @@
 mov.l r8, @-r15
 sts.l pr, @-r15
 
+.macro print_str x_val y_val string
+	!Set the cursor
+	mov.l setCursor, r2
+	mov #\x_val, r4
+	jsr @r2
+	mov #\y_val, r5 !Delay slot (gets executed before the jsr)
+	!Print the string
+	mov.l print, r2
+	mova \string, r0
+	mov r0, r4
+	jsr @r2
+	mov #0, r5 !Delay slot (gets executed before the jsr)
+.endm
+
+!(This was added by SnailMath for hollyhock-2)
+!Check, if the exam mode is not enabled.
+!The battery logo is grey, if exam mode is turned off, it is green or blue (different than grey) when it is turned on.
+!Checking the pixel at x:317 y:507 , it has to be 0x5ACB
+
+mov.l examPixelAddr, r1
+mov.w examColor, r2
+mov.w @r1, r1
+cmp/eq r1, r2
+bt noexam !Don't replace this line with 'bra noexam'. ;-)
+!Execution will continue here if we are in exam mode. (naughty user detected)
+
+!Print error on the screen
+print_str 1,1,line1
+print_str 1,2,line2
+print_str 1,3,line3
+print_str 1,5,line4
+mov.l LCD_Refresh, r2
+jsr @r2
+nop !delay slot
+
+!Wait until the user presses a key
+mov.l waitkey, r2
+jsr @r2
+nop !delay slot
+
+!Reboot the calculator
+mov.l reset, r2
+jmp @r2
+nop !delay slot
+
+
+noexam:
+
+print_str 1,5,file_path !just a test
+mov.l LCD_Refresh, r2
+
 ! Open the file and get a file descriptor
 mova file_path, r0
 mov r0, r4
@@ -85,19 +136,48 @@ buf:
 num_bytes:
 	.long 0x000000FF
 
-.align 4
+.align 2 !The6p4c wrote .align 4, but I think .align 2 works, too.
 open_str:
 	.string "open"
-.align 4
+.align 2
 fstat_str:
 	.string "fstat"
-.align 4
+.align 2
 read_str:
 	.string "read"
-.align 4
+.align 2
 close_str:
 	.string "close"
 
-.align 4	
+.align 2	
 file_path:
 	.string "\\fls0\\run.bin"
+
+
+.align 2
+setCursor:
+.long 0x8002E430
+print:
+.long 0x8002DA0C
+LCD_Refresh:
+.long 0x8003733E
+waitkey:
+.long 0x80094380
+reset:
+.long 0x80000000
+examPixelAddr:
+.long 0x8c000000 + ((317 + (507*320))*2)
+examColor:
+.word 0x5ACB
+.align 2
+line1:
+.string "You can not use the"
+.align 2
+line2:
+.string "hollyhock-2 launcher"
+.align 2
+line3:
+.string "during exam mode!"
+.align 2
+line4:
+.string "Press Clear to reboot."
