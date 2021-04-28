0c1d734d34e463cdfda09fed61210b9466ac028a
SnailMath <snailmathprog@gmail.com>
2021-04-28 10:48:55 +0200

    Update app_template to use .bin format

--- app_template/main.cpp
+++ app_template/main.cpp
@@ -1,10 +1,12 @@
 #include <appdef.hpp>
+#include <sdk/os/lcd.hpp>
+#include <sdk/os/debug.hpp>
 
 /*
  * Fill this section in with some information about your app.
  * All fields are optional - so if you don't need one, take it out.
  */
-APP_NAME("My app name")
+APP_NAME("My app name 2")
 APP_DESCRIPTION("A short description of my app")
 APP_AUTHOR("My name")
 APP_VERSION("1.0.0")
@@ -12,4 +14,7 @@ APP_VERSION("1.0.0")
 extern "C"
 void main() {
     // Put your app's code here!
+	Debug_SetCursorPosition(1,2);
+	Debug_PrintString("HelloWorld",0);
+	LCD_Refresh();
 }
